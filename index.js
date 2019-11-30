f9b0091e32c01be14a191b42120c508688c9df3e
Ambati, Kirankumar <kambati@webmd.net>
2019-11-30 17:02:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
 
-// Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
 
-// Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'bold'
 }
 
-// Text + chalk definitions
 const data = {
   name: chalk.magenta.bold('               Kirankumar Ambati'),
   work: chalk.cyan.bold('Senior Software Engineer @') + chalk.magenta.bold(' ValueLabs'),
@@ -29,7 +25,6 @@ const data = {
   labelCard: chalk.yellow.bold('      Card:')
 }
 
-// Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name}`
 const working = `${data.labelWork}  ${data.work}`
@@ -39,7 +34,6 @@ const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
-// Put all our output together into a single variable so we can use boxen effectively
 const output = heading + newline + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
 
 console.log(chalk.magenta.bold(boxen(output, options)))
